4102b51af2c97cc5175f062b20ccf77313d80d90
Jonas Karg <jonas.karg@hotmail.com>
2019-03-15 21:46:20 +0100

    Matrix multiplication, applyFilter(), normalize(), a little test program.

--- lib.js
+++ lib.js
@@ -84,3 +84,87 @@ function getPatch(inputArray, positionX, positionY, patchSize) {
 
   return patch;
 }
+
+// Multiplies two two-dimensional arrays.
+// Each (y; x) point on one array is multiplied
+// the corresponding point on the other array.
+// Finally, the sum of all points products is
+// calculated
+function matrixMultiply(matrix1, matrix2) {
+  let sum = 0;
+
+  // Validating the cast parameters
+  // => is it possible to multiply those matrices?
+  // if (matrix1.length !== matrix2.length || matrix1[0].length !== matrix2[0].length) {
+  //   throw new Error("Invalid matrix multiplication.");
+  // }
+
+  // The Math
+  for (let y = 0; y < matrix1.length; y++) {
+    for (let x = 0; x < matrix1[0].length; x++) {
+      sum += matrix1[y][x] * matrix2[y][x];
+    }
+  }
+
+  return sum;
+}
+
+// Applies a filter to a two-dimensional array
+function applyFilter(input, filter) {
+  const featureMap = [];
+
+  const width = input[0].length;
+  const height = input.length;
+
+  for (let y = 1; y < height - 1; y++) {
+    featureMap.push([]);
+
+    for (let x = 1; x < width - 1; x++) {
+      const product = matrixMultiply(
+        getPatch(input, x, y, 3),
+        filter
+      );
+
+      featureMap[y - 1][x - 1] = product;
+    }
+  }
+
+  return featureMap;
+}
+
+// Converts a two-dimensional array to a one-dimensional one
+function deCast(matrix) {
+  let array = [];
+
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[0].length; x++) {
+      array.push(matrix[y][x]);
+    }
+  }
+
+  return array;
+}
+
+// Adjusts all values in a matrix to a space between 0 and 1
+function normalize(matrix) {
+  const result = [];
+
+  const width = matrix[0].length;
+  const height = matrix.length;
+  const deCastedArray = deCast(matrix);
+
+  const largest = Math.max.apply(Math, deCastedArray);
+  const smallest = Math.min.apply(Math, deCastedArray);
+
+  const factor = largest - smallest;
+
+  for (let y = 0; y < height; y++) {
+    result.push([]);
+
+    for (let x = 0; x < width; x++) {
+      result[y][x] = (matrix[y][x] - smallest) / factor;
+    }
+  }
+
+  return result;
+}
--- main.js
+++ main.js
@@ -1,13 +1,29 @@
+// const filter = cast([
+//   1, 0, 1,
+//   0, 1, 0,
+//   1, 0, 1,
+// ], 3, 3);
+
 const filter = cast([
-  1, 0, 0,
-  0, 1, 0,
   0, 0, 1,
+  0, 0, 1,
+  1, 1, 1,
 ], 3, 3);
 
 loadImageData("dog.jpg")
   .then(data => {
-    const patch = getPatch(data, 1, 1, 3);
+    const newPixelData = normalize(applyFilter(data, filter), 255);
+    // const newPixelData = data;
 
-    console.log(patch);
-    console.log(data);
-  })
+    const cnv = document.getElementById("cnv");
+    const ctx = cnv.getContext("2d");
+
+    for (let y = 0; y < newPixelData.length; y++) {
+      for (let x = 0; x < newPixelData[0].length; x++) {
+        const brightness = newPixelData[y][x] * 255;
+
+        ctx.fillStyle = `rgb(${ brightness }, ${ brightness }, ${ brightness })`;
+        ctx.fillRect(x, y, 1, 1);
+      }
+    }
+  });
